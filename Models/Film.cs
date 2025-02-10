9f50da44d39607862619a3f598588ec35a5751e8
Samuel Alejandro Hurtado Jimenez <samuel.hurtado@arroyoconsulting.net>
2025-02-09 20:44:03 -0500

    refactor: models location

---
+++ Models/Film.cs
@@ -0,0 +1,13 @@
+﻿namespace Films.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+    }
+}
